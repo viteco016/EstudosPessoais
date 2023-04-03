d1e932088e7312b5e32029326f0a3b00a8a55d88
viteco016 <127994435+viteco016@users.noreply.github.com>
2023-04-03 13:47:19 -0300

    Add files via upload

---
+++ Exercicios/src/estudospessoais/jobrotation/Ex5.java
@@ -0,0 +1,24 @@
+package estudospessoais.jobrotation;
+
+public class Ex5 {
+
+	public static void main(String[] args) {
+// Escreva um programa que inverta os caracteres de um string.
+
+		String original = "invetido";
+
+		char[] caracteres = original.toCharArray();
+
+		
+		for (int i = 0; i < caracteres.length / 2; i++) {
+			char temp = caracteres[i];
+			caracteres[i] = caracteres[caracteres.length - 1 - i];
+			caracteres[caracteres.length - 1 - i] = temp;
+		}
+
+		String invertida = new String(caracteres);
+		
+		System.out.println("Original: " + original);
+		System.out.println("Invertida: " + invertida);
+	}
+}
