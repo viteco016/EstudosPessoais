721550dbceea7c32973e2a5e547def9f7ed67762
viteco016 <127994435+viteco016@users.noreply.github.com>
2023-04-04 10:48:50 -0300

    Add files via upload

---
+++ Exercicios/src/Estudos/JobRotationEx1.java
@@ -0,0 +1,18 @@
+package estudospessoais.Estudos;
+
+public class JobRotationEx1 {
+
+	public static void main(String[] args) {
+		
+		int indice = 13; 
+		int soma = 0;
+		int varK = 0;
+
+		while (varK < indice) {
+			varK = varK + 1;
+			soma = soma + varK;
+		}
+
+		System.out.println(soma);
+	}
+}
